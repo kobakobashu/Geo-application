034b263c1be5b2cb2ec83001156db6932c38a991
kobakobashu <kobaya4@fuji.waseda.jp>
2023-03-17 12:44:49 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 小林周平 on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
